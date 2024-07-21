f98f3705fb7e84542f5d29871d8e4b219dedae19
Ashish Gautam <ashish.gm74@gmail.com>
2024-07-21 20:01:32 +0545

    fix: signup not working fixed

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
 from .forms import SignUpForm
 
 def signup_view(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('login')
+    
+        un = request.POST.get('username')
+        pw = request.POST.get('password1')
+        pw2 = request.POST.get('password2')
+
+        if pw != pw2:
+            print('error not matching password')
+            messages.error(request, 'password don\'t match, try again')
+            return redirect(request.path)
+
+        elif(User.objects.filter(username=un).exists()):
+            print('error userid already exist')
+            messages.error(request, 'User Already Exists, try other unique username')
+            return redirect(request.path)
+
+        else:
+            new_user = User(username=un)
+            new_user.set_password(pw)
+            new_user.save()
+            messages.success(request, 'Account Created Successfully, You can Login Now')
+            return HttpResponseRedirect('/login/')
+     
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
